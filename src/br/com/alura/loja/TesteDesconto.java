c025f6451446ec8f0e48e525c831e773cb7f3b51
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-03-27 23:57:59 -0300

    FEAT(TesteDesconto): Adicionando teste de desconto

---
+++ src/br/com/alura/loja/TesteDesconto.java
@@ -0,0 +1,17 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TesteDesconto {
+    public static void main(String[] args) {
+        Orcamento primeiro = new Orcamento(new BigDecimal("200"),6);
+        Orcamento segundo = new Orcamento(new BigDecimal("200"),3);
+
+        CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+        System.out.println(calculadora.calcular(primeiro));
+        System.out.println(calculadora.calcular(segundo));
+    }
+}
---
+++ src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
@@ -0,0 +1,2 @@
+package br.com.alura.loja.desconto;public class CalculadoraDeDescontos {
+}
