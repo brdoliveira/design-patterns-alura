effe5d093c95acb464e078e198e912aaafde08fe
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-03-30 23:59:16 -0300

    FEAT(Desconto): Adicionando novos métodos abstratos

--- src/br/com/alura/loja/desconto/Desconto.java
+++ src/br/com/alura/loja/desconto/Desconto.java
@@ -12,5 +12,14 @@ abstract class Desconto {
         this.proximo = proximo;
     }
 
-    public abstract BigDecimal calcular(Orcamento orcamento);
+    public BigDecimal calcular(Orcamento orcamento){
+        if(deveAplicar(orcamento)){
+            return efutuarCalculo(orcamento);
+        }
+
+        return proximo.calcular(orcamento);
+    }
+
+    protected abstract BigDecimal efutuarCalculo(Orcamento orcamento);
+    public abstract boolean deveAplicar(Orcamento orcamento);
 }
