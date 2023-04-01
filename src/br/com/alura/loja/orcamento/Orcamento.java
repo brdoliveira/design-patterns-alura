d417f5829a80681b8c64ea6dede529a49e12047a
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-01 03:36:12 -0300

    FEAT(Orcamento): Criando métodos aprovar, reprovar e finalizar

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,16 +1,52 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
     private BigDecimal valor;
     private int quantidadeItens;
+    private SituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
     }
 
-    public BigDecimal getValor() {return valor;}
-    public int getQuantidadeItens() {return quantidadeItens;}
+    public void aplicarDescontoExtra(){
+        BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar(){
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar(){
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar(){
+        this.situacao.finalizar(this);
+    }
+
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public int getQuantidadeItens() {
+        return quantidadeItens;
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao){
+        this.situacao = situacao;
+    }
+
+    public SituacaoOrcamento getSituacao(){
+        return this.situacao;
+    }
 }
