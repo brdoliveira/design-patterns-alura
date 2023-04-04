028f904ef0df2f964965ca6248be55af0a70182d
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-03 23:38:50 -0300

    FEAT(TestesPedidos): Refatorando classe de teste

--- src/br/com/alura/loja/TestesPedidos.java
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -1,11 +1,9 @@
 package br.com.alura.loja;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-import br.com.alura.loja.orcamento.Orcamento;
 import br.com.alura.loja.pedido.GeraPedido;
-import br.com.alura.loja.pedido.Pedido;
+import br.com.alura.loja.pedido.GeraPedidoHandler;
+
+import java.math.BigDecimal;
 
 public class TestesPedidos{
     public static void main(String[] args) {
@@ -14,6 +12,7 @@ public class TestesPedidos{
         int quantidadeItens = Integer.parseInt(args[2]);
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-        gerador.executa();
+        GeraPedidoHandler handler = new GeraPedidoHandler(/*dependencias*/);
+        handler.executa(gerador);
     }
 }
