f47ea64609fc8dc5d6fd7c8b5f3a6bb7b5a853f7
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-03 23:58:40 -0300

    FEAT(TestesPedidos): Refatorando codigo

--- src/br/com/alura/loja/TestesPedidos.java
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -2,8 +2,11 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class TestesPedidos{
     public static void main(String[] args) {
@@ -12,7 +15,12 @@ public class TestesPedidos{
         int quantidadeItens = Integer.parseInt(args[2]);
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-        GeraPedidoHandler handler = new GeraPedidoHandler(/*dependencias*/);
+        GeraPedidoHandler handler = new GeraPedidoHandler(
+                Arrays.asList(
+                        new SalvarPedidoNoBancoDeDados(),
+                        new EnviarEmailPedido()
+                )
+        );
         handler.executa(gerador);
     }
 }
