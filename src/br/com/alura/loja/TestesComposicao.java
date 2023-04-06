fdf32ed9b8da4778fe0a604632a9f92eba0841f3
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-05 23:56:12 -0300

    FEAT(TestesComposicao): Testando Proxy

--- src/br/com/alura/loja/TestesComposicao.java
+++ src/br/com/alura/loja/TestesComposicao.java
@@ -2,6 +2,7 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.orcamento.OrcamentoProxy;
 
 import java.math.BigDecimal;
 
@@ -9,9 +10,16 @@ public class TestesComposicao {
     public static void main(String[] args) {
         Orcamento antigo = new Orcamento();
         antigo.adicionarItem(new ItemOrcamento(new BigDecimal("200")));
-        antigo.reprovar();
 
         Orcamento novo = new Orcamento();
         novo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
+        novo.adicionarItem(antigo);
+
+        Orcamento proxy = new OrcamentoProxy(novo);
+
+        System.out.println(proxy.getValor());
+        System.out.println(proxy.getValor());
+        System.out.println(proxy.getValor());
+        System.out.println(proxy.getValor());
     }
 }
